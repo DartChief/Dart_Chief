26b64753b3a5d1e34eb881c3e5fd5b8145d0b8c1
Даниил <dartchief@yandex.ru>
2015-03-17 20:00:20 +0300

    Task 1 without FilippHernya

--- spring-property-management-for-hibernate/src/main/java/com/kzn/itis/SpringExampleProperties.java
+++ spring-property-management-for-hibernate/src/main/java/com/kzn/itis/SpringExampleProperties.java
@@ -11,12 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
 
 /**
  *
@@ -36,6 +33,30 @@ public class SpringExampleProperties {
         this.config = config;
     }
 
+    public static String insertOrders(int id, int CustomerId,
+                                      int SalersPersonalId, int TotalAmount, Statement stmt) {
+        String sql = "INSERT INTO ORDERS VALUES (" + id + "," + CustomerId
+                + "," + SalersPersonalId + "," + TotalAmount + ")";
+        try {
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return sql;
+    }
+
+    public static String insertStudents(int id, String LastName,
+                                        String FirstName, Statement stmt) {
+        String sql = "INSERT INTO STUDENTS VALUES (" + id + ",'" + LastName
+                + "','" + FirstName + "')";
+        try {
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return sql;
+    }
+
     public void run() throws SQLException {
         logger.info("Welcome to Example Application");
         logger.info("url=" + config.getDbUrl());
@@ -48,18 +69,39 @@ public class SpringExampleProperties {
         configOverrides.put("hibernate.hbm2ddl.auto", config.getDbHbm2ddl());
         SessionUtil.getEntityManagerFactory(configOverrides);
 
-        InputStream fis = SpringExampleProperties.class.getResourceAsStream("/derby.properties");
-        Properties prop = new Properties();
-        try {
-            prop.load(fis);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         Connection con = null;
         try {
             Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
-            con = DriverManager.getConnection(prop.getProperty(config.getDbUrl()));
-            testDB();
+            con = DriverManager.getConnection(config.getDbUrl());
+            Statement stmt = con.createStatement();
+            String sql1 = "CREATE TABLE ORDERS (" + "Id INTEGER not NULL,"
+                    + " CustomerId INTEGER not NULL,"
+                    + "SalersPersonalId INTEGER not NULL,"
+                    + "TotalAmount INTEGER not NULL," + " PRIMARY KEY (Id))";
+            stmt.executeUpdate(sql1);
+            insertOrders(1, 1, 5, 200, stmt);
+            insertOrders(2, 2, 6, 300, stmt);
+            insertOrders(3, 3, 7, 400, stmt);
+            String sql2 = "CREATE TABLE STUDENTS (" + "id INTEGER not NULL,"
+                    + " LastName VARCHAR(256),"
+                    + " FirstName VARCHAR(256), PRIMARY KEY (id))";
+            stmt.executeUpdate(sql2);
+            insertStudents(1, "Fam1", "Name1", stmt);
+            insertStudents(2, "Fam2", "Name2", stmt);
+            insertStudents(3, "Fam3", "Name3", stmt);
+            insertStudents(4, "Fam4", "Name4", stmt);
+            insertStudents(5, "Fam5", "Name5", stmt);
+            insertStudents(6, "Fam6", "Name6", stmt);
+            insertStudents(7, "Fam7", "Name7", stmt);
+            String sql = "SELECT s.LastName || ' ' || s.FirstName Name, o.TotalAmount FROM STUDENTS s LEFT JOIN Orders o ON o.Customerid = s.id";
+            ResultSet res = stmt.executeQuery(sql);
+            while (res.next()) {
+                System.out.println(res.getString(1) + " " + res.getString(2));
+            }
+            String sql3 = "DROP TABLE STUDENTS";
+            stmt.executeUpdate(sql3);
+            String sql4 = "DROP TABLE ORDERS";
+            stmt.executeUpdate(sql4);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -67,7 +109,7 @@ public class SpringExampleProperties {
             con.close();
         }
 
-
+       //testDB();
     }
 
     protected void testDB() {
